4dd9772e1fe05ee4ecc1a866dcb6eaefd59c3534
Manfred Bergmann <mb@software-by-mabe.com>
2020-09-02 22:42:00 +0200

    added support for OCaml through dune

--- tcr-mode.el
+++ tcr-mode.el
@@ -23,6 +23,18 @@
       (message "test call result: %s" call-result)
       call-result)))
 
+(defun execute-ocaml-test ()
+  "Call OCaml test via 'dune'."
+  (let* ((test-cmd-args (list "dune" "test"))
+         (call-args
+          (append (list (car test-cmd-args) nil "TCR out" t)
+                  (cdr test-cmd-args))))
+    (message "calling: %s" call-args)
+    (let* ((default-directory (locate-dominating-file default-directory "dune-project"))
+           (call-result (apply 'call-process call-args)))
+      (message "test call result: %s" call-result)
+      call-result)))
+
 (defun execute-git-cmd (git-cmd)
   "Call the given GIT-CMD with 'shell-command."
   (message "calling: %s" git-cmd)
@@ -35,6 +47,8 @@
   (let ((test-result (cond
                       ((string-equal "elixir-mode" major-mode)
                        (execute-elixir-test))
+                      ((string-equal "tuareg-mode" major-mode)
+                       (execute-ocaml-test))
                       (t (progn (message "Unknown mode!")
                                 nil)))))
 
@@ -57,4 +71,5 @@
             map))
 
 (provide 'tcr-mode)
+
 ;;; tcr-mode ends here
